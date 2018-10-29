c541ccdf77d08cbd376ba928cfa6970cd952cdd4
Unknown <teslaruzero@gmail.com>
2018-10-29 10:55:39 +0100

    Add Gem Skeleton

---
+++ lib/policy/manager.rb
@@ -0,0 +1,7 @@
+require "policy/manager/version"
+
+module Policy
+  module Manager
+    # Your code goes here...
+  end
+end
---
+++ lib/policy/manager/version.rb
@@ -0,0 +1,5 @@
+module Policy
+  module Manager
+    VERSION = "0.1.0"
+  end
+end
