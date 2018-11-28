ec37b1d5f8651cc898afac7f1f37d72396a9f51f
Nicolae Teslaru <teslaruzero@gmail.com>
2018-11-28 16:46:38 +0100

    Fix Portability Request Logs

--- app/models/policy_manager/concerns/works_with_job.rb
+++ app/models/policy_manager/concerns/works_with_job.rb
@@ -45,38 +45,10 @@ module PolicyManager
 
       def mark_as_complete
         self.update(job_completed_at: DateTime.now)
-        self.log_portability_requests(PolicyManager::Concerns::WorksWithJob::JobStatues::COMPLETED)
       end
 
       def mark_as_failed
         self.update(job_failed_at: DateTime.now)
-        self.log_portability_requests(PolicyManager::Concerns::WorksWithJob::JobStatues::FAILED)
-      end
-
-      def log_portability_requests(state)
-        case state
-        when PolicyManager::Concerns::WorksWithJob::JobStatues::PROCESSING
-          Log.create(
-            log_type: Log::LogTypes::INFO,
-            description: "[PortabilityRequest] Created portability request #{self.id}",
-            logable: self,
-            user_id: self.user_id
-          )
-        when PolicyManager::Concerns::WorksWithJob::JobStatues::FAILED
-          Log.create(
-            log_type: Log::LogTypes::ERROR,
-            description: "[PortabilityRequest] Portability request #{self.id} failed",
-            logable: self,
-            user_id: self.user_id
-          )
-        when PolicyManager::Concerns::WorksWithJob::JobStatues::COMPLETED
-          Log.create(
-            log_type: Log::LogTypes::INFO,
-            description: "[PortabilityRequest] Portability request #{self.id} completed successfully",
-            logable: self,
-            user_id: self.user_id
-          )
-        end
       end
     end
   end
--- app/models/policy_manager/portability_request.rb
+++ app/models/policy_manager/portability_request.rb
@@ -9,6 +9,7 @@ module PolicyManager
     has_many :logs, as: :logable
 
     after_create -> { log_portability_requests(PolicyManager::Concerns::WorksWithJob::JobStatues::PROCESSING) }
+    after_save :log_after_save
 
     has_attached_file :attachment,
                       path: ':rails_root/private/policy_manager/portability_requests/:id/:basename.:extension',
@@ -18,5 +19,36 @@ module PolicyManager
     do_not_validate_attachment_file_type :attachment
 
     scope :of_user, ->(user) { where(user_id: user.id) }
+    
+    def log_after_save
+      log_portability_requests(PolicyManager::Concerns::WorksWithJob::JobStatues::COMPLETED) if self.job_completed?
+      log_portability_requests(PolicyManager::Concerns::WorksWithJob::JobStatues::FAILED) if self.job_failed?
+    end
+
+    def log_portability_requests(state)
+      case state
+      when PolicyManager::Concerns::WorksWithJob::JobStatues::PROCESSING
+        Log.create(
+          log_type: Log::LogTypes::INFO,
+          description: "Created portability request #{self.id}",
+          logable: self,
+          user_id: self.user_id
+        )
+      when PolicyManager::Concerns::WorksWithJob::JobStatues::FAILED
+        Log.create(
+          log_type: Log::LogTypes::ERROR,
+          description: "Portability request #{self.id} failed",
+          logable: self,
+          user_id: self.user_id
+        )
+      when PolicyManager::Concerns::WorksWithJob::JobStatues::COMPLETED
+        Log.create(
+          log_type: Log::LogTypes::INFO,
+          description: "Portability request #{self.id} completed successfully",
+          logable: self,
+          user_id: self.user_id
+        )
+      end
+    end
   end
 end
